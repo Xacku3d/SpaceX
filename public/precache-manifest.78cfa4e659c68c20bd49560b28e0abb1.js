e8b47fb930e00195ae05fce4478f5fd2f3c303f2
Xacku3d <41637751+Xacku3d@users.noreply.github.com>
2019-06-14 21:50:46 +0300

    build

---
+++ public/precache-manifest.78cfa4e659c68c20bd49560b28e0abb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ffaabc277e82f6ddcd061a4c1649ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccd0e739046fe60bf0b7",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "312791b389bf4c684270",
+    "url": "/static/js/2.17f32f58.chunk.js"
+  },
+  {
+    "revision": "ccd0e739046fe60bf0b7",
+    "url": "/static/js/main.a0a093c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
